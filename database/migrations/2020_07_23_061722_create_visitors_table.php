4690fa863d528b84925c147511eaf1300e831d50
Misha En <mishaenn@gmail.com>
2020-07-23 09:21:53 +0300

    make event model and add visitor and event migration

--- database/migrations/2020_07_23_061722_create_visitors_table.php
+++ database/migrations/2020_07_23_061722_create_visitors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateVisitorTable extends Migration
+class CreateVisitorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,17 @@ class CreateVisitorTable extends Migration
      */
     public function up()
     {
-        Schema::create('visitor', function (Blueprint $table) {
+        Schema::create('visitors', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('event_id')->index();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
             $table->timestamps();
+        });
+
+        Schema::table('visitors', function(Blueprint $table){
+           $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
         });
     }
 
